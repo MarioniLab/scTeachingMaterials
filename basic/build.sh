2c94887dc354c0c284676b74b2b6d05bddce4714
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-05-09 10:43:20 +0100

    Automated basic workshop set-up.

--- basic/build.sh
+++ basic/build.sh
@@ -1,3 +1,5 @@
 cat workshop_kolod.Rmd | grep -v "^#A#" | sed "s/^#H# //" > practical.Rmd
 cat workshop_kolod.Rmd | grep -v "^#H#" | sed "s/^#A# //" > answers.Rmd
 
+echo "rmarkdown::render('prepare.Rmd')" | R --slave --no-save
+rm prepare.Rmd
