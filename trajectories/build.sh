987da85bdc3803df1b7b056f5139a9d4a4e44165
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-05-20 16:28:26 +0100

    Fixed error in builder script.

--- trajectories/build.sh
+++ trajectories/build.sh
@@ -1,6 +1,6 @@
-cat workshop_unbatch.Rmd | grep -v "^#A#" | sed "s/^#H# //" > practical.Rmd
-cat workshop_unbatch.Rmd | grep -v "^#H#" | sed "s/^#A# //" > answers.Rmd
-rm workshop_unbatch.Rmd
+cat workshop_trajectories.Rmd | grep -v "^#A#" | sed "s/^#H# //" > practical.Rmd
+cat workshop_trajectories.Rmd | grep -v "^#H#" | sed "s/^#A# //" > answers.Rmd
+rm workshop_trajectories.Rmd
 
 rm answers.md
 rm test.sh
